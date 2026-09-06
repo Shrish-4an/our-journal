21f24c9cd7bfab36447158efb06bb633971847bf
Shrish-4an <ishaanv8060@gmail.com>
2026-09-06 16:46:47 +0530

    Update Supabase Key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ezeewzyktxqoqdhvfdpn.supabase.co";
-const supabaseAnonKey = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6ZWV3enlrdHhnb3FkaHZmZHBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2ODY5NDAsImV4cCI6MjEwNDI2Mjk0MH0.XCVnJPzJPvhHUsncvpDFr80SRwdvkOYVwr5etPQ17E0";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
